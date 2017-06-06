b04efcb3970f7bb649f66fa46cc8f3e7fd198990
dukesrg <dukesrg@gmail.com>
2017-06-06 23:30:29 +0300

    draft elf patches parser updated

--- tools/elfparse.c
+++ tools/elfparse.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "elf.h"
 
 void main(int argc, char *argv[]){
@@ -24,16 +25,32 @@ void main(int argc, char *argv[]){
 
 	Elf32_Shdr *sectionheader = (Elf32_Shdr*)&data[elfheader->e_shoff];
 
-	for(size_t shnum = 0; shnum < elfheader->e_shnum; shnum++){
-                char *name = &data[sectionheader[elfheader->e_shstrndx].sh_offset + sectionheader[shnum].sh_name];
-		printf("Section #%d\n\tname:\t%s\n\tdata:\t", shnum, name);
-                if(name[0] == '.')
+	uint32_t symbolssection = 0;
+	uint32_t symbolnamessection = 0;
+	char *sectionnames = &data[sectionheader[elfheader->e_shstrndx].sh_offset];
+	for(size_t i = 0; i < elfheader->e_shnum; i++){
+                char *sectionname = &sectionnames[sectionheader[i].sh_name];
+		printf("Section #%d\n\tname:\t%s\n\tdata:\t", i, sectionname);
+                if(*sectionname == '.'){
 			printf("...");
-		else
-	                for(size_t idx = 0; idx < sectionheader[shnum].sh_size; idx++)
-				printf("%02X ", data[sectionheader[shnum].sh_offset + idx]);
+			if(!strcmp(sectionname, ".symtab"))
+				symbolssection = i;
+                        else if(!strcmp(sectionname, ".strtab"))
+				symbolnamessection = i;
+		}else
+	                for(size_t idx = 0; idx < sectionheader[i].sh_size; idx++)
+				printf("%02X ", data[sectionheader[i].sh_offset + idx]);
 		printf("\n");
 	}
+
+	printf("Symbols:\n");
+
+	Elf32_Sym *symbolheader = (Elf32_Sym*)&data[sectionheader[symbolssection].sh_offset];
+	uint32_t symbolscount = sectionheader[symbolssection].sh_size / sizeof(Elf32_Sym);
+	char *symbolnames = &data[sectionheader[symbolnamessection].sh_offset];
+	for(size_t i = 0; i < symbolscount; i++)
+                if(symbolheader[i].st_info == 0) //skip non-locals
+			printf("%s - %s = %08X\n", &sectionnames[sectionheader[symbolheader[i].st_shndx].sh_name] , &symbolnames[symbolheader[i].st_name], symbolheader[i].st_value);
 
 	return;
 }
