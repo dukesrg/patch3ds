c928734393225637d8b6ee68c879cd8979fbf21d
dukesrg <dukesrg@gmail.com>
2017-06-09 01:23:26 +0300

    Library functions ready

--- include/patch.h
+++ include/patch.h
@@ -9,12 +9,13 @@ typedef struct {
 	void *data;
 } __attribute__((packed)) patch_record;
 
-uint16_t sectionFind(Elf32_Shdr *sections, uint_fast16_t count, char *section_names, char *name);
+//check elf magic and prepare data for operations, number of available patch sets returned
+uint_fast16_t patchPreload(void *data);
 
 //get avaiable patch names for specified Title ID and Tile Version (or 0xFFFF for any version)
-uint_fast16_t patchGetNames(char **patch_names, Elf32_Sym *symbols, uint16_t symbols_count, char *symbol_names, Elf32_Shdr *sections, uint_fast16_t sections_count, char *section_names, uint64_t title_id, uint_fast16_t title_version);
+uint_fast16_t patchFind(char **patch_names, uint_fast16_t count, uint64_t title_id, uint_fast16_t title_version);
 
 //get patch data record for specified patch names and Title ID and Title Version combination
-uint_fast16_t patchGetData(patch_record *patch_data, char **patch_names, Elf32_Sym *symbols, uint_fast16_t symbols_count, char *symbol_names, Elf32_Shdr *sections, uint_fast16_t sections_count, char *section_names, uint64_t title_id, uint_fast16_t title_version);
+uint_fast16_t patchGet(patch_record *patch_data, char **patch_names, uint_fast16_t count, uint64_t title_id, uint_fast16_t title_version);
 
 #endif
--- source/elfparse.c
+++ source/elfparse.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 #include "elf.h"
 #include "patch.h"
@@ -18,40 +16,26 @@ void main(int argc, char *argv[]){
 	fread(data, sizeof data, 1, fp);
 	fclose(fp);
 
-	Elf32_Ehdr *elfheader = (Elf32_Ehdr*)data;
-
-        if((*(uint32_t*)&elfheader->e_ident) != *(uint32_t*)ELFMAG){
-		printf("ELF magic not found!");
+	uint_fast16_t max_patches = patchPreload(data);
+        if (!max_patches) {
+		printf("Invalid ELF file or no patch sections found!");
 		return;
 	}
 
-	Elf32_Shdr *sections = (Elf32_Shdr*)(data + elfheader->e_shoff);
-	uint_fast16_t sections_count = elfheader->e_shnum;
-	char *section_names = (char*)(data + sections[elfheader->e_shstrndx].sh_offset);
-	uint_fast16_t symbol_section = sectionFind(sections, sections_count, section_names, ".symtab");
-	char *symbol_names = (char*)(data + sections[sectionFind(sections, sections_count, section_names, ".strtab")].sh_offset);
-        Elf32_Sym *symbols = (Elf32_Sym*)(data + sections[symbol_section].sh_offset);
-
-	uint32_t symbols_count = sections[symbol_section].sh_size / sizeof(*symbols);
-
 	printf("Patches avaiable for o3DS:\n");	
-	char *o3ds_patch_names[sections_count];
-        memset(o3ds_patch_names, 0, sections_count * sizeof(*o3ds_patch_names));
-	patchGetNames(o3ds_patch_names, symbols, symbols_count, symbol_names, sections, sections_count, section_names, 0x0004013800000002, 0xFFFF);
-	patchGetNames(o3ds_patch_names, symbols, symbols_count, symbol_names, sections, sections_count, section_names, 0x0004013800000102, 0xFFFF);
-	patchGetNames(o3ds_patch_names, symbols, symbols_count, symbol_names, sections, sections_count, section_names, 0x0004013800000202, 0xFFFF);
-	
-	for (size_t i = 0; o3ds_patch_names[i]; printf("\t%s\n", o3ds_patch_names[i++]));
+	char *patch_names[max_patches];
+	uint_fast16_t num_patches = 0;
+	num_patches = patchFind(patch_names, num_patches, 0x0004013800000002, 0xFFFF);
+	num_patches = patchFind(patch_names, num_patches, 0x0004013800000102, 0xFFFF);
+	num_patches = patchFind(patch_names, num_patches, 0x0004013800000202, 0xFFFF);
+	for (size_t i = 0; i < num_patches; printf("\t%s\n", patch_names[i++]));
 
 	printf("Patch data from the above patch names compatible with o3DS 11.4 NATIVE_FIRM:\n");	
-	patch_record o3ds_patch_data[sections_count];
-        memset(o3ds_patch_data, 0, sections_count * sizeof(*o3ds_patch_data));
-	patchGetData(o3ds_patch_data, o3ds_patch_names, symbols, symbols_count, symbol_names, sections, sections_count, section_names, 0x0004013800000002, 0x6F60);
-	for (size_t i = 0; o3ds_patch_data[i].data; i++) {
-		printf("\t%08X: ", o3ds_patch_data[i].address);
-		for (size_t j = 0; j < o3ds_patch_data[i].size; printf(" %02X", *(uint8_t*)(data + (uint32_t)o3ds_patch_data[i].data + (j++))));
+	patch_record patch_data[num_patches];
+	uint_fast16_t num_data = patchGet(patch_data, patch_names, num_patches, 0x0004013800000002, 0x6F60);
+	for (size_t i = 0; i < num_data; i++) {
+		printf("\t%08X: ", patch_data[i].address);
+		for (size_t j = 0; j < patch_data[i].size; printf(" %02X", *(uint8_t*)(patch_data[i].data + (j++))));
 		printf("\n");
 	}
-
-	return;
 }
